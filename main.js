92844b087d7b041374fe3cd5c1773cf406048136
Fouad <fouadezari@gmail.com>
2022-09-01 01:56:19 +0200

    feat add mobile navigation

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 console.log('Hey 🤓')
+const menu= document.querySelector('.toggle')
+const navigation= document.querySelector('.navigation')
+
+menu.addEventListener('click', () => {
+  menu.classList.toggle('active')
+  navigation.classList.toggle('active')
+})
