4a9cebdc80a95c5455e70cbdcb1d62b33261e405
Fouad <fouadezari@gmail.com>
2022-09-05 23:40:42 +0200

    feat fixed header to top and add more sections

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 console.log('Hey 🤓')
 const menu= document.querySelector('.toggle')
 const navigation= document.querySelector('.navigation')
+const navLink = document.querySelectorAll(".nav-link");
 
 menu.addEventListener('click', () => {
   menu.classList.toggle('active')
   navigation.classList.toggle('active')
 })
+
+
+navLink.forEach((link) => link.addEventListener("click", () =>{
+  menu.classList.remove("active")
+  navigation.classList.remove("active")
+}));
